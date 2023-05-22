a300101c75cf33957a65e44ae5896ec3491244c3
Nhan Tran <rim25102004@gmail.com>
2023-05-22 21:50:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  
+  var [job,setJob] = useState('');
+  var [jobs,setJobs] = useState(()=>{
+    var prevSavedJob = JSON.parse(localStorage.getItem('jobs'));
+    return prevSavedJob;
+  } );
+  function handleSubmit(){
+    if(job !== ''){
+      setJob('')
+      setJobs(function(prevJobs){
+        var newJobs = [...prevJobs,job]
+        var JSONjobs = JSON.stringify(newJobs)
+        localStorage.setItem('jobs',JSONjobs)
+        return newJobs
+      })
+    }
+  }
+  function handleDelete(deleteIndex){
+    setJobs(function(prevs){
+      var oldList = [...prevs]
+      var newList = oldList.filter(function(item,index){
+        return item !== jobs[deleteIndex]
+      })
+      localStorage.setItem('jobs',JSON.stringify(newList))
+      return newList
+    })
+  }
+  return <div style={{padding : 32}}>
+    <input
+      value={job}
+      onChange={function(e){setJob(e.target.value)}}
+    ></input>
+    <button onClick={handleSubmit}>Add</button>
+    <ul>
+      {
+        jobs.map(function(job,index){
+          return <li key={index}><span>{job}</span><button style={{margin : 10}} onClick={function(){return handleDelete(index)}}>Delete</button></li>
+        })
+      }
+    </ul>
+  </div>
+}
 export default App;
