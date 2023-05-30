596cfcfe86f074be0abb08861dbe2b308970947d
Nhan Tran <rim25102004@gmail.com>
2023-05-30 10:28:52 +0700

    Aplying with basic Up down number with useReducer

--- src/App.js
+++ src/App.js
@@ -1,60 +1,49 @@
 
-import { useState, useRef, useMemo } from "react";
+import { useState, useRef, useMemo, useReducer } from "react";
+
+// useState
+// 1. init state: 0
+// 2. Actions : Up(state + 1) Down (state - 1)
+
+// usereducer
+// 1. init state: 0
+// 2. Actions : Up(state + 1) Down (state - 1)
+// 3. Reducer
+// 4. dispatch( a term which is used to activate an action)
+
+// init state
+const initState = 0
+
+// Actions
+const UP_ACTION = 'up'
+const DOWN_ACTION = 'down'
+
+// Reducer 
+const reducer=(state, action)=> {
+  switch(action){
+    case UP_ACTION:
+      return state + 1
+    case DOWN_ACTION:
+      return state - 1
+    default:
+      throw new Error(`Invalid action`)
+  }
+}
+
 
 function App() {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [products, setProducts] = useState([]);
-
-  const nameInput = useRef();
-
-  const handleSubmit = () => {
-    setProducts((prev) => {
-      return [...prev, { name, price: +price }];
-    });
-    setName("");
-    setPrice("");
-    nameInput.current.focus();
-  };
-  // bấm add thì nó mới tính toán lại (thay đổi Products thì nó mới tính toán lại)
-  const total = useMemo(() =>{
-    const result = products.reduce(
-     (result, product) => {
-      console.log('tính toán lại......')
-
-     return result + product.price},
-      0
-    );
-    return result;
-  }, [products]);
-  // nếu để Dependencies rỗng thì nó chỉ trả kết quả một lần là kết quả đã tính toán trước đó và không update lại cái mới
-  // Dependencies được chuyền vào thay đổi thì tính toán lại không thì không re-render lại
+  const [count, dispatch]=useReducer(reducer, initState)
 
   return (
     <div style={{ padding: 40 }}>
-      <input
-        ref={nameInput}
-        placeholder="Enter name..."
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <br />
-      <input
-        placeholder="Enter price..."
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      <br />
-      <button onClick={handleSubmit}>Add</button>
-      <br />
-      Total: {total}
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            {product.name} - {product.price}
-          </li>
-        ))}
-      </ul>
+      <h1>{count}</h1>
+      <button
+      onClick={()=>{dispatch(DOWN_ACTION)}}
+      >Down</button>
+      <button
+      onClick={()=>{dispatch(UP_ACTION)}}
+      >Up</button>
+
     </div>
   );
 }
